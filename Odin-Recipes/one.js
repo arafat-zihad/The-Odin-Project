1313577f4213f4bc4e31fd6e2fe1506ce888bc7a
arafat-zihad <arafat.zihad01@gmail.com>
2026-06-20 16:50:55 +0600

    Add sumAllNums function and tempCodeRunnerFile for date formatting

---
+++ Odin-Recipes/one.js
@@ -0,0 +1,16 @@
+// function declaration
+
+function sumAllNums() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+
+console.log(sumAllNums(1, 2, 3, 4)); // 10
+console.log(sumAllNums(10, 20, 13, 40, 10)); // 93
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40)); // 173
+
+
+
