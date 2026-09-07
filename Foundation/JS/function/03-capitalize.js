187fd72f93408d64e773bd183c848af1b23317c3
arafat-zihad <arafat.zihad01@gmail.com>
2026-09-08 01:07:53 +0600

    Add capitalize function to transform string case

---
+++ Foundation/JS/function/03-capitalize.js
@@ -0,0 +1,7 @@
+function capitalize(value){
+    result = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+    return result;
+}
+
+console.log(capitalize("kiTabBa asOnI baLa?"))
+console.log(capitalize("ABcD"))
