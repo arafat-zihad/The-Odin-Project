a5bc4b0e638b4ed44786ecff3e6148aed52b4f40
arafat-zihad <arafat.zihad01@gmail.com>
2026-09-10 02:26:06 +0600

    Add filter and map array methods examples in separate files

---
+++ Foundation/JS/array/filter.js
@@ -0,0 +1,4 @@
+const arr = [1, 2, 3, 4, 5];
+
+const oddNums = arr.filter((num) => num % 2 !== 0);
+console.log(oddNums)
---
+++ Foundation/JS/array/map.js
@@ -0,0 +1,24 @@
+// V1 - without map
+arr = [1, 2, 3, 4, 5]
+
+function sumOfTripleEvens(array)
+{
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            const tripleEvenNo = arr[i] * 3;
+            sum += tripleEvenNo;
+        }
+    }
+    return sum;
+}
+// console.log(sumOfTripleEvens())
+
+
+//  map()
+
+// function addOne(num){
+//     return num + 1;
+// }
+const mappedArr = arr.map((num) => num + 1);
+console.log(mappedArr)
